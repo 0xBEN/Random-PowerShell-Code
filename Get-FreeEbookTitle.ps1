0713607985a6befe425bd23755ea643b7e33973d
root <root@proxmox.home.lab>
2021-04-24 18:00:48 -0400

    Remove email functionality from script.

--- Get-FreeEbookTitle.ps1
+++ Get-FreeEbookTitle.ps1
@@ -1,9 +1,6 @@
 $InformationPreference = 'SilentlyContinue'
 $VerbosePreference = 'SilentlyContinue'
 $ProgressPreference = 'SilentlyContinue'
-$credentials = Import-Clixml "$PSScriptRoot/EncryptedCredentials/gmail.clixml"
-$smtpServer = 'smtp.gmail.com'
-$port = 587
 $subject = "Today's Free Packt eBook"
 $modulesToImport = '/root/PSModules/PSToolbox'
 $uri = 'https://www.packtpub.com/free-learning'
@@ -16,16 +13,6 @@ $plaintextWebhookUri = $encryptedWebhookUri | ConvertFrom-SecureString
 $request = Invoke-WebRequest $uri
 $eBook = $request.Content | Remove-HtmlTags | Select-String '^Free.eBook.*'
 
-$emailBody = 
-@"
-<html>
-<body>
-	<p><strong><a href=`"$uri`">Today's Free eBook (must click link to claim, must be read on publisher page -- no download)</a></strong>:<br>$eBook</p>
-    <p><em>This email was sent by a cron job on your Proxmox server.</p>
-</body>
-</html>
-"@
-
 $discordBody = @"
 **__Today's Free Packt E-Book:__**
 *This e-book must be read using the publisher's online reader, no downloads*
@@ -34,16 +21,5 @@ $discordBody = @"
 **Title:** $ebook
 "@
 
-Send-MailMessage `
--UseSsl `
--From $credentials.UserName `
--To $credentials.UserName `
--Subject $subject `
--Body $emailBody `
--BodyAsHtml `
--SmtpServer $smtpServer `
--Port $port `
--Credential $credentials `
--WarningAction SilentlyContinue
-
 & "$PSScriptRoot/Send-DiscordWebhook.ps1" -webhook_uri $plaintextWebhookUri -username $botName -content $discordBody -avatar_url $discordBotLogo
+
